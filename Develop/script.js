0cdee592eb2e2529bc9e20d75079958be5b07d59
jessicap5159 <jessicaj5159@gmail.com>
2021-03-01 14:21:55 -0800

    added current date to header

---
+++ Develop/script.js
@@ -0,0 +1,7 @@
+
+function date_time() {
+    now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    document.getElementById('currentDay').innerHTML = now;
+    setTimeout(function () { date_time(); }, 1000);
+  }
+  date_time();
