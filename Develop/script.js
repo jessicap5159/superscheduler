ab6fd4af6d7e94b62b99fae90b209b8e08350a69
jessicap5159 <jessicaj5159@gmail.com>
2021-03-02 13:57:31 -0800

    added ability to edit task

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,18 @@
-
+// Display date and time in header
 function date_time() {
     now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
     document.getElementById('currentDay').innerHTML = now;
     setTimeout(function () { date_time(); }, 1000);
-  }
-  date_time();
- 
+}
+date_time();
+// Edit task ** need to fix row width ** 
+$(".col-11").on("click", "p", function () {
+    var text = $(this)
+        .text()
+        .trim();
+    var textInput = $("<textarea>")
+        .addClass("event-edit")
+        .val(text);
+        $(this).replaceWith(textInput);
 
-
-  // test
+});
