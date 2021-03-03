2e9b654ef566713ad3fb04f772ea471b7d72088c
jessicap5159 <jessicaj5159@gmail.com>
2021-03-03 13:41:50 -0800

    pseudocode for saving and color-coding

--- Develop/script.js
+++ Develop/script.js
@@ -5,32 +5,15 @@ function date_time() {
     setTimeout(function () { date_time(); }, 1000);
 }
 date_time();
-// Edit task ** need to fix row width ** 
-//$(".col-11").on("click", "p", function () {
-//     var text = $(this)
-//         .text()
-//         .trim();
-//         console.log(text);
 
-//     var textInput = $("<textarea>")
-//         .addClass("event-edit")
-//         .val(text);
-//         $(this).replaceWith(textInput);
-//         textInput.trigger("focus");
+// Pseudocode for saving tasks:
+// user enters/edits task and clicks 'save' button
+// data is sent to localStorage
+// data is retrieved from localStorage
+// data is displayed on page and persists when page is reloaded
 
-// });
-
-// let page = document.querySelector('.container');
-// let timeArr = ['9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm'];
-// function timeBlock(arr) {
-//     for ( let i = 0; i < arr.length; i++) {
-//         let div = document.createElement('div');
-//         div.setAttribute('class', 'gum')
-//         let divText = document.createTextNode(arr[i]);
-//         let form = document.createElement('input');
-//         div.appendChild(divText);
-//         div.appendChild(form)
-//         page.appendChild(div)
-//     }
-// }
-// timeBlock(timeArr);
+// Pseudocode for color-coding:
+// user views time blocks
+// current hour is red
+// past hours are gray
+// future hours are green
