d76223ea9f219c16d6df0919fd64c8d04b39b323
jessicap5159 <jessicaj5159@gmail.com>
2021-03-03 12:24:33 -0800

    changed grid layout

--- Develop/script.js
+++ Develop/script.js
@@ -20,23 +20,17 @@ date_time();
 
 // });
 
-
-
-
-
-
-
-let page = document.querySelector('.container');
-let timeArr = ['9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm'];
-function timeBlock(arr) {
-    for ( let i = 0; i < arr.length; i++) {
-        let div = document.createElement('div');
-        div.setAttribute('class', 'gum')
-        let divText = document.createTextNode(arr[i]);
-        let form = document.createElement('input');
-        div.appendChild(divText);
-        div.appendChild(form)
-        page.appendChild(div)
-    }
-}
-timeBlock(timeArr)
+// let page = document.querySelector('.container');
+// let timeArr = ['9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm'];
+// function timeBlock(arr) {
+//     for ( let i = 0; i < arr.length; i++) {
+//         let div = document.createElement('div');
+//         div.setAttribute('class', 'gum')
+//         let divText = document.createTextNode(arr[i]);
+//         let form = document.createElement('input');
+//         div.appendChild(divText);
+//         div.appendChild(form)
+//         page.appendChild(div)
+//     }
+// }
+// timeBlock(timeArr);
--- README.md
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-# Work Day Scheduler Starter Code
-=======
-# superscheduler
->>>>>>> 3311ad15236f5e27cc2828def49804eaadf536cf
