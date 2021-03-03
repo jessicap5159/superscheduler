d432845d5c102bd002ce38f902682adeabdce9c6
jessicap5159 <jessicaj5159@gmail.com>
2021-03-02 16:08:03 -0800

    fixed input fields for time slots

--- Develop/script.js
+++ Develop/script.js
@@ -6,14 +6,37 @@ function date_time() {
 }
 date_time();
 // Edit task ** need to fix row width ** 
-$(".col-11").on("click", "p", function () {
-    var text = $(this)
-        .text()
-        .trim();
-    var textInput = $("<textarea>")
-        .addClass("event-edit")
-        .val(text);
-        $(this).replaceWith(textInput);
-        textInput.trigger("focus");
+//$(".col-11").on("click", "p", function () {
+//     var text = $(this)
+//         .text()
+//         .trim();
+//         console.log(text);
 
-});
+//     var textInput = $("<textarea>")
+//         .addClass("event-edit")
+//         .val(text);
+//         $(this).replaceWith(textInput);
+//         textInput.trigger("focus");
+
+// });
+
+
+
+
+
+
+
+let page = document.querySelector('.container');
+let timeArr = ['9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm'];
+function timeBlock(arr) {
+    for ( let i = 0; i < arr.length; i++) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'gum')
+        let divText = document.createTextNode(arr[i]);
+        let form = document.createElement('input');
+        div.appendChild(divText);
+        div.appendChild(form)
+        page.appendChild(div)
+    }
+}
+timeBlock(timeArr)
