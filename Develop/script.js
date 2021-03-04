d7787ee19c473147628b88c1933e69daea66f903
jessicap5159 <jessicaj5159@gmail.com>
2021-03-04 12:04:16 -0800

    added functions to assign time and store and display in localstorage

--- Develop/script.js
+++ Develop/script.js
@@ -7,13 +7,55 @@ function date_time() {
 date_time();
 
 // Pseudocode for saving tasks:
-// user enters/edits task and clicks 'save' button
-// data is sent to localStorage
-// data is retrieved from localStorage
-// data is displayed on page and persists when page is reloaded
+// user enters/edits task and clicks 'save' button... onclick event?
+// data is sent to localStorage... setitem
+// data is retrieved from localStorage ...getitem
+// data is displayed on page and persists when page is reloaded ...JSON.parse(window.localStorage.getItem('x'));
 
 // Pseudocode for color-coding:
-// user views time blocks
+// user views time blocks - audit task? convert to momemt object? remove old class and apply new?
 // current hour is red
 // past hours are gray
 // future hours are green
+
+var currentTime = moment();
+
+var timeCounter = moment("9 am", "hh A");
+
+
+// Function to assign time to each span
+$(".time").each(function(){
+    $(this).text(timeCounter.format("hh A"));
+    timeCounter = timeCounter.add(1, "hours");
+    var spanTime = moment($(this).text(), "hh A");
+    
+    if (spanTime.hour() < currentTime.hour()) {
+        $(this).next("input").addClass("past");
+    } else if (spanTime.hour() === currentTime.hour()) {
+            $(this).next("input").addClass("present");
+    } else { 
+        $(this).next("input").addClass("future");
+    }
+
+});
+
+// Function to store in local storage and display
+$(".save").on("click", function() {
+    var timeSlot = $(this).prev("input").prev("span")[0].outerText;
+    var myEvent = $(this).prev("input")[0].value;
+    localStorage.setItem(timeSlot, myEvent);
+    
+});
+
+   function displayStorage() { 
+    $("span").each(function(){
+    var displayEvent = localStorage.getItem($(this)[0].outerText);
+     if (displayEvent !=null) {
+        $(this).next("input")[0].value = displayEvent;
+     } else { 
+         $(this).next("input")[0].value = "";
+    
+     }
+   });
+}
+displayStorage();
